d75dc29daa6d7568d3043aba4ca240bb19ab39ed
Alberto Abella <alberto.abella@fiware.org>
2023-04-14 15:40:34 +0200

    test of SQL export

---
+++ ElectricGenerator/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model ElectricGenerator of the subject dataModel.S4BLDG for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE ElectricGenerator_type AS ENUM ('ElectricGenerator');
+CREATE TABLE ElectricGenerator (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, electricGeneratorEfficiency text, hasManufacturer text, hasModel text, id text, isContainedInBuildingSpace json, isContainedInPhysicalObject json, isSubSystemOf json, location json, name text, owner json, powerOutputMax text, seeAlso json, source text, startCurrentFactor text, type ElectricGenerator_type);
