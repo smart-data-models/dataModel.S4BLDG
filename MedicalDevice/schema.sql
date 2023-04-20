5ef0ef9ee1cf503a82ca51307d26546cc3a96fce
Alberto Abella <alberto.abella@fiware.org>
2023-04-20 18:42:52 +0200

    test of SQL export

---
+++ MedicalDevice/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model MedicalDevice of the subject dataModel.S4BLDG for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE MedicalDevice_type AS ENUM ('MedicalDevice');
+CREATE TABLE MedicalDevice (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasManufacturer text, hasModel text, id text, isContainedInBuildingSpace json, isContainedInPhysicalObject json, isSubSystemOf json, location json, name text, owner json, seeAlso json, source text, type MedicalDevice_type);
