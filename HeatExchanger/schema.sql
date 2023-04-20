01af4eef827dc18f5fed1973a7ba1bcd65cbcb04
Alberto Abella <alberto.abella@fiware.org>
2023-04-20 18:42:45 +0200

    test of SQL export

---
+++ HeatExchanger/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model HeatExchanger of the subject dataModel.S4BLDG for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE HeatExchanger_type AS ENUM ('HeatExchanger');
+CREATE TABLE HeatExchanger (address json, alternateName text, areaServed text, arrangement text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasManufacturer text, hasModel text, id text, isContainedInBuildingSpace json, isContainedInPhysicalObject json, isSubSystemOf json, location json, name text, owner json, seeAlso json, source text, type HeatExchanger_type);
