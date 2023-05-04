9b57850b47bfc85268f48bf2cd23c3391c67ebeb
Alberto Abella <alberto.abella@fiware.org>
2023-05-04 02:07:17 +0200

    test of SQL export

---
+++ UnitaryControlElement/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model UnitaryControlElement of the subject dataModel.S4BLDG for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE UnitaryControlElement_type AS ENUM ('UnitaryControlElement');
+CREATE TABLE UnitaryControlElement (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasManufacturer text, hasModel text, id text, isContainedInBuildingSpace json, isContainedInPhysicalObject json, isSubSystemOf json, location json, name text, owner json, seeAlso json, source text, type UnitaryControlElement_type);
