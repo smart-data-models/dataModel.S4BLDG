3018b2d61d5ed2006ab95e7d9add806557c03d0e
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:08:28 +0200

    test of SQL export

--- SolarDevice/schema.sql
+++ SolarDevice/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model SolarDevice of the subject dataModel.S4BLDG for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model SolarDevice of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE SolarDevice_type AS ENUM ('SolarDevice');
-CREATE TABLE SolarDevice (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasManufacturer text, hasModel text, id text, isContainedInBuildingSpace json, isContainedInPhysicalObject json, isSubSystemOf json, location json, name text, owner json, seeAlso json, source text, type SolarDevice_type);
+CREATE TABLE SolarDevice (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, isSubSystemOf JSON, name TEXT, owner JSON, source TEXT, type SolarDevice_type);
