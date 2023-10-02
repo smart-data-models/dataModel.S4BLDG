2d7854845cb8d6d1c60372dd4d9d83a36f0b2891
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:06:58 +0200

    test of SQL export

--- Building/schema.sql
+++ Building/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Building of the subject dataModel.S4BLDG for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Building of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Building_type AS ENUM ('Building');
-CREATE TABLE Building (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type Building_type);
+CREATE TABLE Building (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Building_type);
