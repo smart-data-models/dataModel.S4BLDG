6d70a82049cbc14c2384d923ffee68e4f12a9f52
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:07:59 +0200

    test of SQL export

--- FlowMeter/schema.sql
+++ FlowMeter/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model FlowMeter of the subject dataModel.S4BLDG for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model FlowMeter of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE FlowMeter_type AS ENUM ('FlowMeter');
-CREATE TABLE FlowMeter (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasManufacturer text, hasModel text, id text, isContainedInBuildingSpace json, isContainedInPhysicalObject json, isSubSystemOf json, location json, name text, owner json, readOutType text, remoteReading text, seeAlso json, source text, type FlowMeter_type);
+CREATE TABLE FlowMeter (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, isSubSystemOf JSON, name TEXT, owner JSON, readOutType TEXT, remoteReading BOOLEAN, source TEXT, type FlowMeter_type);
