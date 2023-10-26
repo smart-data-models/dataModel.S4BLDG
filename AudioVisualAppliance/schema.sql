2d5a82eaa4e93284c6f554a3251db884b28bcb2c
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:17:25 +0200

    test of SQL export

--- AudioVisualAppliance/schema.sql
+++ AudioVisualAppliance/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model AudioVisualAppliance of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AudioVisualAppliance_type AS ENUM ('AudioVisualAppliance');
-CREATE TABLE AudioVisualAppliance (address JSON, alternateName TEXT, areaServed TEXT, audioVolume NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, isSubSystemOf JSON, mediaSource TEXT, name TEXT, owner JSON, source TEXT, type AudioVisualAppliance_type);
+CREATE TABLE AudioVisualAppliance (address JSON, alternateName TEXT, areaServed TEXT, audioVolume NUMERIC, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, id TEXT PRIMARY KEY, isSubSystemOf JSON, location JSON, mediaSource TEXT, name TEXT, owner JSON, seeAlso JSON, source TEXT, type AudioVisualAppliance_type);
