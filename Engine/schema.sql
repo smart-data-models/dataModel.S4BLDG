5427faa16c57b25f192e6126c4cebc5f7e9d58b5
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:18:13 +0200

    test of SQL export

--- Engine/schema.sql
+++ Engine/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Engine of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Engine_type AS ENUM ('Engine');
-CREATE TABLE Engine (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, energySource TEXT, hasManufacturer TEXT, hasModel TEXT, isSubSystemOf JSON, name TEXT, owner JSON, source TEXT, type Engine_type);
+CREATE TABLE Engine (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, energySource TEXT, hasManufacturer TEXT, hasModel TEXT, id TEXT PRIMARY KEY, isSubSystemOf JSON, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Engine_type);
