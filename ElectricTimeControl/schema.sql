5ab5c09eed0a490cf0158e8c968c432f83be09f0
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:18:11 +0200

    test of SQL export

--- ElectricTimeControl/schema.sql
+++ ElectricTimeControl/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ElectricTimeControl of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ElectricTimeControl_type AS ENUM ('ElectricTimeControl');
-CREATE TABLE ElectricTimeControl (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, isSubSystemOf JSON, name TEXT, owner JSON, source TEXT, type ElectricTimeControl_type);
+CREATE TABLE ElectricTimeControl (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, id TEXT PRIMARY KEY, isSubSystemOf JSON, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type ElectricTimeControl_type);
