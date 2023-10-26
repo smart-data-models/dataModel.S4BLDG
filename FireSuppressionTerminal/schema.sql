664126ecf27dd6d482b559f93da4579461040ee1
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:18:25 +0200

    test of SQL export

--- FireSuppressionTerminal/schema.sql
+++ FireSuppressionTerminal/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model FireSuppressionTerminal of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE FireSuppressionTerminal_type AS ENUM ('FireSuppressionTerminal');
-CREATE TABLE FireSuppressionTerminal (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, isSubSystemOf JSON, name TEXT, owner JSON, source TEXT, type FireSuppressionTerminal_type);
+CREATE TABLE FireSuppressionTerminal (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, id TEXT PRIMARY KEY, isSubSystemOf JSON, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type FireSuppressionTerminal_type);
