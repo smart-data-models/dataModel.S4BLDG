e7a3d3edee45f4a497ba709bf32cd9090ada1e11
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:17:22 +0200

    test of SQL export

--- Alarm/schema.sql
+++ Alarm/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Alarm of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Alarm_type AS ENUM ('Alarm');
-CREATE TABLE Alarm (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, isSubSystemOf JSON, name TEXT, owner JSON, source TEXT, type Alarm_type);
+CREATE TABLE Alarm (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasManufacturer TEXT, hasModel TEXT, id TEXT PRIMARY KEY, isSubSystemOf JSON, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Alarm_type);
