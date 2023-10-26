28d6589495dfb6418f658ee8be53616f745638f7
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:17:34 +0200

    test of SQL export

--- Burner/schema.sql
+++ Burner/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Burner of the subject dataModel.S4BLDG for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Burner_type AS ENUM ('Burner');
-CREATE TABLE Burner (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, energySource TEXT, hasManufacturer TEXT, hasModel TEXT, isSubSystemOf JSON, name TEXT, owner JSON, source TEXT, type Burner_type);
+CREATE TABLE Burner (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, energySource TEXT, hasManufacturer TEXT, hasModel TEXT, id TEXT PRIMARY KEY, isSubSystemOf JSON, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Burner_type);
