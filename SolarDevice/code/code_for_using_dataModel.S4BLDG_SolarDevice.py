d1c72792ae2e6a93fb201038e6d0d3bbe76916b9
Alberto Abella <alberto.abella@fiware.org>
2024-05-16 01:26:41 +0200

    created example of code

---
+++ SolarDevice/code/code_for_using_dataModel.S4BLDG_SolarDevice.py
@@ -0,0 +1,61 @@
+
+#         # This code allows you to install a orion-ld broker in a linux system
+#         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
+#         
+#         # INSTALL NGSI LD broker (OrionLD)
+#         sudo docker pull mongo:3.6
+#         sudo docker pull fiware/orion-ld
+#         sudo docker network create fiware_default
+#         sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles
+#         sudo docker run -d --name fiware-orionld -h orion --network=fiware_default -p 1026:1026  fiware/orion-ld -dbhost mongo-db
+#         
+#         # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#         sudo docker stop fiware-orionld
+#         sudo docker rm fiware-orionld
+#         sudo docker stop mongo-db
+#         sudo docker rm mongo-db
+#         sudo docker network rm fiware_default
+#         
+#         # CHECK INSTANCES
+#         # Check the broker is running
+#         curl -X GET 'http://localhost:1026/version'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#         
+#         # now the python code you can use to insert some value in the context broker according to the data model
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
+dataModel = "SolarDevice"
+subject = "dataModel.S4BLDG"
+isContainedInBuildingSpace = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:BuildingSpace:a7992d2d-19f4-4e6b-938f-9de8314e6a31'}"
+attribute = "isContainedInBuildingSpace"
+value = isContainedInBuildingSpace
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+isContainedInPhysicalObject = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:PhysicalObject:cd9cf384-7e05-44b2-816b-9bc094ad8423'}"
+attribute = "isContainedInPhysicalObject"
+value = isContainedInPhysicalObject
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+isSubSystemOf = [{'type': 'Relationship', 'object': 'urn:ngsi-ld:System:fea01066-da6c-4823-8331-a7450f139df6'}, {'type': 'Relationship', 'object': 'urn:ngsi-ld:System:4f04da6b-d1c0-47a7-afd9-4455b3cfda01'}, {'type': 'Relationship', 'object': 'urn:ngsi-ld:System:353b9e43-78b1-44b7-a3e4-8f9bc523d7fc'}]
+attribute = "isSubSystemOf"
+value = isSubSystemOf
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+hasManufacturer = "{'type': 'Property', 'value': 'SolarDevice Company Inc.'}"
+attribute = "hasManufacturer"
+value = hasManufacturer
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
