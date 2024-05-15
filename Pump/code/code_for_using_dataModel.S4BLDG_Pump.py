d328813043e334552d4ca76f292d8b493cd93967
Alberto Abella <alberto.abella@fiware.org>
2024-05-16 01:26:31 +0200

    created example of code

---
+++ Pump/code/code_for_using_dataModel.S4BLDG_Pump.py
@@ -0,0 +1,61 @@
+
+#         # This code allows you to install a orion-ld broker in a linux system
+#         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
+#         
+#         # INSTALL NGSI LD broker (OrionLD)
+#         sudo docker pull mongo:3.6
+#         sudo docker pull fiware/orion-ld
+#         sudo docker network create fiware_default
+#         sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles
+#         sudo docker run -d --name fiware-orionld -h orion --network=fiware_default -p 1026:1026  fiware/orion-ld -dbhost mongo-db
+#         
+#         # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#         sudo docker stop fiware-orionld
+#         sudo docker rm fiware-orionld
+#         sudo docker stop mongo-db
+#         sudo docker rm mongo-db
+#         sudo docker network rm fiware_default
+#         
+#         # CHECK INSTANCES
+#         # Check the broker is running
+#         curl -X GET 'http://localhost:1026/version'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#         
+#         # now the python code you can use to insert some value in the context broker according to the data model
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
+dataModel = "Pump"
+subject = "dataModel.S4BLDG"
+connectionSize = {'type': 'Property', 'unitCode': 'mm', 'observedAt': '2023-01-26T01:56:40Z', 'value': 0.439871049852415}
+attribute = "connectionSize"
+value = connectionSize
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+flowResistanceMax = {'type': 'Property', 'unitCode': 'N/m2', 'observedAt': '2023-01-26T10:30:54Z', 'value': 0.70272326323097}
+attribute = "flowResistanceMax"
+value = flowResistanceMax
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+flowResistanceMin = {'type': 'Property', 'unitCode': 'N/m2', 'observedAt': '2023-01-26T11:23:10Z', 'value': 0.748100252355086}
+attribute = "flowResistanceMin"
+value = flowResistanceMin
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+netPositiveSuctionHead = {'type': 'Property', 'unitCode': 'N/m2', 'observedAt': '2023-01-25T23:42:12Z', 'value': 0.4372375818125598}
+attribute = "netPositiveSuctionHead"
+value = netPositiveSuctionHead
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
