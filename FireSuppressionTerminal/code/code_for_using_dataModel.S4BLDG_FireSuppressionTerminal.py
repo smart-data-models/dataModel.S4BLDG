1df3db717f6d1a64953a9bddac9cc8bcb116aa34
Alberto Abella <alberto.abella@fiware.org>
2024-05-16 01:25:50 +0200

    created example of code

---
+++ FireSuppressionTerminal/code/code_for_using_dataModel.S4BLDG_FireSuppressionTerminal.py
@@ -0,0 +1,61 @@
+
+#         # This code allows you to install a orion-ld broker in a linux system
+#         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
+#         
+#         # INSTALL NGSI LD broker (OrionLD)
+#         sudo docker pull mongo:3.6
+#         sudo docker pull fiware/orion-ld
+#         sudo docker network create fiware_default
+#         sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles
+#         sudo docker run -d --name fiware-orionld -h orion --network=fiware_default -p 1026:1026  fiware/orion-ld -dbhost mongo-db
+#         
+#         # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#         sudo docker stop fiware-orionld
+#         sudo docker rm fiware-orionld
+#         sudo docker stop mongo-db
+#         sudo docker rm mongo-db
+#         sudo docker network rm fiware_default
+#         
+#         # CHECK INSTANCES
+#         # Check the broker is running
+#         curl -X GET 'http://localhost:1026/version'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#         
+#         # now the python code you can use to insert some value in the context broker according to the data model
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
+dataModel = "FireSuppressionTerminal"
+subject = "dataModel.S4BLDG"
+isContainedInBuildingSpace = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:BuildingSpace:114ef098-ce1a-4472-9dde-920ce363a235'}"
+attribute = "isContainedInBuildingSpace"
+value = isContainedInBuildingSpace
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+isContainedInPhysicalObject = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:PhysicalObject:368214d7-9133-43b9-a039-cfc29a776bd3'}"
+attribute = "isContainedInPhysicalObject"
+value = isContainedInPhysicalObject
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+isSubSystemOf = [{'type': 'Relationship', 'object': 'urn:ngsi-ld:System:b021ab2b-22e3-4138-8476-ec6b4cc3f50d'}, {'type': 'Relationship', 'object': 'urn:ngsi-ld:System:6463b606-5b54-4d7f-9e94-6cfd6fc02f65'}, {'type': 'Relationship', 'object': 'urn:ngsi-ld:System:ada26427-12e2-4d5b-ab7b-0756e7a0ab09'}]
+attribute = "isSubSystemOf"
+value = isSubSystemOf
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+hasManufacturer = "{'type': 'Property', 'value': 'FireSuppressionTerminal Company Inc.'}"
+attribute = "hasManufacturer"
+value = hasManufacturer
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
