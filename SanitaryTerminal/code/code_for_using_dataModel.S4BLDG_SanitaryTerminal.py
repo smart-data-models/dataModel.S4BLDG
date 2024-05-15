4331e6283e423e5f4772e71dd479c3eb2c67f633
Alberto Abella <alberto.abella@fiware.org>
2024-05-16 01:26:34 +0200

    created example of code

---
+++ SanitaryTerminal/code/code_for_using_dataModel.S4BLDG_SanitaryTerminal.py
@@ -0,0 +1,61 @@
+
+#         # This code allows you to install a orion-ld broker in a linux system
+#         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
+#         
+#         # INSTALL NGSI LD broker (OrionLD)
+#         sudo docker pull mongo:3.6
+#         sudo docker pull fiware/orion-ld
+#         sudo docker network create fiware_default
+#         sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles
+#         sudo docker run -d --name fiware-orionld -h orion --network=fiware_default -p 1026:1026  fiware/orion-ld -dbhost mongo-db
+#         
+#         # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#         sudo docker stop fiware-orionld
+#         sudo docker rm fiware-orionld
+#         sudo docker stop mongo-db
+#         sudo docker rm mongo-db
+#         sudo docker network rm fiware_default
+#         
+#         # CHECK INSTANCES
+#         # Check the broker is running
+#         curl -X GET 'http://localhost:1026/version'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#         
+#         # now the python code you can use to insert some value in the context broker according to the data model
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
+dataModel = "SanitaryTerminal"
+subject = "dataModel.S4BLDG"
+isContainedInBuildingSpace = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:BuildingSpace:a391c678-15b7-4570-a954-958436fbef48'}"
+attribute = "isContainedInBuildingSpace"
+value = isContainedInBuildingSpace
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+isContainedInPhysicalObject = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:PhysicalObject:b133d789-668b-4b6f-b42f-8906a2f30e78'}"
+attribute = "isContainedInPhysicalObject"
+value = isContainedInPhysicalObject
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+isSubSystemOf = [{'type': 'Relationship', 'object': 'urn:ngsi-ld:System:0556162c-4448-45c2-9920-28199c38d32a'}, {'type': 'Relationship', 'object': 'urn:ngsi-ld:System:31b4154c-ca94-4b6d-b8f8-c29b9d6bef77'}, {'type': 'Relationship', 'object': 'urn:ngsi-ld:System:1e790182-a4ea-4b40-a4ab-2611b896d90b'}]
+attribute = "isSubSystemOf"
+value = isSubSystemOf
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+hasManufacturer = "{'type': 'Property', 'value': 'SanitaryTerminal Company Inc.'}"
+attribute = "hasManufacturer"
+value = hasManufacturer
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
