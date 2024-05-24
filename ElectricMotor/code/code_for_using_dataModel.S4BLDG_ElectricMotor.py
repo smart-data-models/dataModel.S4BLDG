79710557bb72636dbb6e1c16e4a9dd0f4aa66a34
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:49:57 +0200

    created example of code

--- ElectricMotor/code/code_for_using_dataModel.S4BLDG_ElectricMotor.py
+++ ElectricMotor/code/code_for_using_dataModel.S4BLDG_ElectricMotor.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ElectricMotor"
 subject = "dataModel.S4BLDG"
-electricMotorEfficiency = {'type': 'Property', 'unitCode': 'NA', 'observedAt': '2023-01-26T08:26:24Z', 'value': 0.07012980187189011}
+electricMotorEfficiency = 0.36137565435400376
 attribute = "electricMotorEfficiency"
 value = electricMotorEfficiency
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-frameSize = "{'type': 'Property', 'value': 'Saint Martin'}"
+frameSize = "Awesome"
 attribute = "frameSize"
 value = frameSize
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hasPartWinding = {'type': 'Property', 'value': False}
+hasPartWinding = False
 attribute = "hasPartWinding"
 value = hasPartWinding
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-isGuarded = {'type': 'Property', 'value': False}
+isGuarded = False
 attribute = "isGuarded"
 value = isGuarded
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
