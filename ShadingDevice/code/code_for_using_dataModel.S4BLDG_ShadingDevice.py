e15e16111473535218e37c4d6ae6934a91276c95
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:51:11 +0200

    created example of code

--- ShadingDevice/code/code_for_using_dataModel.S4BLDG_ShadingDevice.py
+++ ShadingDevice/code/code_for_using_dataModel.S4BLDG_ShadingDevice.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ShadingDevice"
 subject = "dataModel.S4BLDG"
-isExternal = {'type': 'Property', 'value': True}
+isExternal = False
 attribute = "isExternal"
 value = isExternal
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-mechanicalOperated = {'type': 'Property', 'value': True}
+mechanicalOperated = True
 attribute = "mechanicalOperated"
 value = mechanicalOperated
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-roughness = "{'type': 'Property', 'value': 'Practical Rubber Cheese'}"
+roughness = "Executive"
 attribute = "roughness"
 value = roughness
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-shadingDeviceType = "{'type': 'Property', 'value': 'parsing'}"
+shadingDeviceType = "client-driven"
 attribute = "shadingDeviceType"
 value = shadingDeviceType
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
