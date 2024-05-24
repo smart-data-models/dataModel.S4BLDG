649dd09bcb1bb601482c45997a503a2f66b9dd9b
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:49:39 +0200

    created example of code

--- Damper/code/code_for_using_dataModel.S4BLDG_Damper.py
+++ Damper/code/code_for_using_dataModel.S4BLDG_Damper.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Damper"
 subject = "dataModel.S4BLDG"
-airFlowRateMax = {'type': 'Property', 'unitCode': 'm3/s', 'observedAt': '2023-01-26T10:15:08Z', 'value': 0.46010915943742847}
+airFlowRateMax = 0.5927918101987754
 attribute = "airFlowRateMax"
 value = airFlowRateMax
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-bladeAction = "{'type': 'Property', 'value': 'microchip'}"
+bladeAction = "Belize Dollar"
 attribute = "bladeAction"
 value = bladeAction
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-bladeEdge = "{'type': 'Property', 'value': 'Village'}"
+bladeEdge = "frictionless"
 attribute = "bladeEdge"
 value = bladeEdge
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-bladeShape = "{'type': 'Property', 'value': 'Netherlands Antillian Guilder'}"
+bladeShape = "intermediate"
 attribute = "bladeShape"
 value = bladeShape
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
