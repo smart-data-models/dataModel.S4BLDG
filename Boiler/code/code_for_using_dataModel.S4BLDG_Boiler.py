3b6dd6d8f9df4d683f31f5a1701636d616f6b95f
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:48:53 +0200

    created example of code

--- Boiler/code/code_for_using_dataModel.S4BLDG_Boiler.py
+++ Boiler/code/code_for_using_dataModel.S4BLDG_Boiler.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Boiler"
 subject = "dataModel.S4BLDG"
-energySource = "{'type': 'Property', 'value': 'Sri Lanka'}"
+energySource = "United States of America"
 attribute = "energySource"
 value = energySource
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-heatTransferSurfaceArea = {'type': 'Property', 'unitCode': 'm2', 'observedAt': '2023-01-25T19:16:12Z', 'value': 0.4182368568397056}
+heatTransferSurfaceArea = 0.7853344933267346
 attribute = "heatTransferSurfaceArea"
 value = heatTransferSurfaceArea
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-isWaterStorageHeater = {'type': 'Property', 'value': False}
+isWaterStorageHeater = False
 attribute = "isWaterStorageHeater"
 value = isWaterStorageHeater
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-nominalEnergyConsumption = {'type': 'Property', 'unitCode': 'J/s', 'observedAt': '2023-01-25T23:06:57Z', 'value': 0.5252685918504294}
+nominalEnergyConsumption = 0.6203749662936422
 attribute = "nominalEnergyConsumption"
 value = nominalEnergyConsumption
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
