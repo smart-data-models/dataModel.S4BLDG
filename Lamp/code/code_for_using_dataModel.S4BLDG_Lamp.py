97f99a81905818a32f614b1433c8f3fb5247823c
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:50:44 +0200

    created example of code

--- Lamp/code/code_for_using_dataModel.S4BLDG_Lamp.py
+++ Lamp/code/code_for_using_dataModel.S4BLDG_Lamp.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Lamp"
 subject = "dataModel.S4BLDG"
-colorAppearance = "{'type': 'Property', 'value': 'card'}"
+colorAppearance = "Washington"
 attribute = "colorAppearance"
 value = colorAppearance
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-colorRenderingIndex = {'type': 'Property', 'value': 0.6745960848595047}
+colorRenderingIndex = 0.8153696255721333
 attribute = "colorRenderingIndex"
 value = colorRenderingIndex
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-colorTemperature = {'type': 'Property', 'unitCode': 'K', 'observedAt': '2023-01-26T05:53:48Z', 'value': 0.03839635886669124}
+colorTemperature = 0.09664075512365078
 attribute = "colorTemperature"
 value = colorTemperature
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-contributedLuminousFlux = {'type': 'Property', 'unitCode': 'Steradian', 'observedAt': '2023-01-26T12:44:07Z', 'value': 0.43828304543957874}
+contributedLuminousFlux = 0.9207573270583412
 attribute = "contributedLuminousFlux"
 value = contributedLuminousFlux
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
